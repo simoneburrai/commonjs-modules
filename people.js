51f998dfe9d1ff4ad92487facd85c1f7ca094631
simoneburrai <burrai.s98@gmail.com>
2025-03-24 16:07:19 +0100

    feat: created people object with imported functions

--- people.js
+++ people.js
@@ -1,4 +1,14 @@
-const fullName = require("./names.js");
-console.log(fullName("Simone", "Burrai"));
+const createName = require("./names.js");
+console.log(createName("Simone", "Burrai"));
 const hobbiesList = require("./hobbies.js");
 console.log(hobbiesList("calcio", "nuoto", "football"));
+
+const person = () => {
+    personInfo = {
+        fullName:  createName("Simone", "Burrai"),
+        hobbies: hobbiesList("basket", "skate", "guitar")
+    }
+    return personInfo;
+}
+
+console.log(person());
